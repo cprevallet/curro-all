5a9878e651b33e764170f0046914f9e921502e0b
Craig Prevallet <penguintx@hotmail.com>
2026-01-17 18:36:31 -0600

    Rename the translation files to avoid contention when installing both aggregate and siliconsneaker2 on Debian.

--- src/i18n.rs
+++ src/i18n.rs
@@ -13,9 +13,9 @@ thread_local! {
 
     // Load the appropriate file based on the detected language
     let ftl_content = match lang_id.language.as_str() {
-        "es" => include_str!("../i18n/es/gui.ftl"),
-        "fr" => include_str!("../i18n/fr/gui.ftl"),
-        _ => include_str!("../i18n/en-US/gui.ftl"),
+        "es" => include_str!("../i18n/es/gui_aggregate.ftl"),
+        "fr" => include_str!("../i18n/fr/gui_aggregate.ftl"),
+        _ => include_str!("../i18n/en-US/gui_aggregate.ftl"),
     };
 
     let resource = FluentResource::try_new(ftl_content.to_string())
